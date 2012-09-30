4d883e07ac0f781a75cb44780119bd6670957844
yoshirou.ozeki <yoshirou.ozeki@mixi.co.jp>
2012-09-30 17:12:54 +0900

    first commit

---
+++ .vim/pr_menu_diary_iframe.pl
@@ -0,0 +1,25 @@
+#!/usr/bin/perl 
+#===============================================================================
+#
+#         FILE:  pr_menu_diary_iframe.pl
+#
+#        USAGE:  ./pr_menu_diary_iframe.pl 
+#
+#  DESCRIPTION:  
+#
+#      OPTIONS:  ---
+# REQUIREMENTS:  ---
+#         BUGS:  ---
+#        NOTES:  ---
+#       AUTHOR:   (), <>
+#      COMPANY:  
+#      VERSION:  1.0
+#      CREATED:  2008年12月05日 16時56分17秒 JST
+#     REVISION:  ---
+#===============================================================================
+
+use strict;
+use warnings;
+
+
+
